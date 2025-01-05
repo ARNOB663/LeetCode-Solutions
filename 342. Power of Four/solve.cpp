5f2da86f59ee78ab16a0428ce1f276f194ee095a
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-01-05 13:29:21 +0600

    Update solve.cpp

--- 342. Power of Four/solve.cpp
+++ 342. Power of Four/solve.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cmath>
-
+using namespace std;
 class Solution
 {
 public:
@@ -8,29 +8,22 @@ public:
     {
         if (n <= 0)
             return false;
-
         // Calculate the logarithm base 4 of n using the change of base formula
-        double logBase4 = log(n) / log(4); // log(n) / log(4) = log4(n)
-
-
+        double logBase4 = log(n) / log(4);
         // Check if the result is an integer
         return floor(logBase4) == logBase4;  // If the floor of the result is equal to the result, then it is an integer
     }
 };
-
 int main()
 {
     Solution solution;
-
-    // Test cases
     int test1 = 16; // Expected output: true
     int test2 = 5;  // Expected output: false
     int test3 = 1;  // Expected output: true
-
-    // Printing the results
-    std::cout << "Is " << test1 << " a power of four? " << (solution.isPowerOfFour(test1) ? "true" : "false") << std::endl;
-    std::cout << "Is " << test2 << " a power of four? " << (solution.isPowerOfFour(test2) ? "true" : "false") << std::endl;
-    std::cout << "Is " << test3 << " a power of four? " << (solution.isPowerOfFour(test3) ? "true" : "false") << std::endl;
+    // Function call
+    cout << solution.isPowerOfFour(test1) << endl;
+    cout << solution.isPowerOfFour(test2) << endl;
+    cout << solution.isPowerOfFour(test3) << endl;
 
     return 0;
 }
