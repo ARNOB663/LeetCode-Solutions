2deb12e5287eb7720f9265361846ca31c3b82b87
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-01-07 19:53:04 +0600

    Update solve.js

--- 268. Missing Number/solve.js
+++ 268. Missing Number/solve.js
@@ -21,7 +21,6 @@ class Solution {
         for (let i = 0; i <= n; i++) {
             if (hash[i] === 0) {
                 return i;
-            
             }
         }
         return -1; // Default case (should not occur with valid input)
@@ -31,15 +30,11 @@ class Solution {
 const solution = new Solution();
 const nums1 = [3, 0, 1];
 console.log("Missing number:", solution.missingNumber(nums1)); // Output: 2
-
 const nums2 = [0, 1];
 console.log("Missing number:", solution.missingNumber(nums2)); // Output: 2
-
 const nums3 = [9, 6, 4, 2, 3, 5, 7, 0, 1];
 console.log("Missing number:", solution.missingNumber(nums3)); // Output: 8
-
 const nums4 = [0];
 console.log("Missing number:", solution.missingNumber(nums4)); // Output: 1
-
 const nums5 = [0, 2];
 console.log("Missing number:", solution.missingNumber(nums5)); // Output: 1
