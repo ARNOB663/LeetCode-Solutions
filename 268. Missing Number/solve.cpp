5fb499a15eb25a99310ba99a26b37bab88f8630b
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-01-10 21:16:00 +0600

    Update solve.cpp

--- 268. Missing Number/solve.cpp
+++ 268. Missing Number/solve.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <numeric>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<numeric>
 using namespace std;
 
 class Solution {
