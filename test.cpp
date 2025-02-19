a8727f5404df40fc333dbb1b7e6d80b4883e057b
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-02-19 20:18:25 +0600

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+       string s;
+       if(s[s.length()-1]=="s" && s[s.length()-2] == "u")
+       {
+        
+       }
+       
+    }
+}
