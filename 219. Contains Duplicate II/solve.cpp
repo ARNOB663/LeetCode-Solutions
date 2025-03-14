336c4caec90cdd953c138746c0f3a9777fa70dfc
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-03-15 00:37:47 +0600

    Update solve.cpp

--- 219. Contains Duplicate II/solve.cpp
+++ 219. Contains Duplicate II/solve.cpp
@@ -10,6 +10,22 @@ class Solution {
                 }
            }
            return false;
-        }
-        
+        }  
     };
+
+    //more optimized version 
+    class Solution {
+        public:
+            bool containsNearbyDuplicate(vector<int>& nums, int k) { 
+                unordered_map<int, int> mpp;
+                for (int i = 0; i < nums.size(); ++i) {
+                    if (mpp.count(nums[i])) {
+                        if (i - mpp[nums[i]] <= k) {
+                            return true;
+                        }
+                    }
+                    mpp[nums[i]] = i;
+                }
+                return false;
+            }
+        };
