a15cdde101a7d3cde1845bfdfd2b794df7d03605
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-04-17 01:06:08 +0600

    optimal solution

--- 2149. Rearrange Array Elements by Sign/solve.cpp
+++ 2149. Rearrange Array Elements by Sign/solve.cpp
@@ -6,6 +6,7 @@ class Solution {
         vector<int>pos;
         vector<int>neg;
         vector<int>arr;
+        int n= nums.size();
     
         for(int i=0;i<nums.size();i++)
         {
@@ -20,7 +21,6 @@ class Solution {
         int p=0,n=0;
         for(int i=0;i<nums.size();i++)
         {
-         
          if(i%2==0)
          {
             arr.push_back(pos[p++]);
@@ -30,6 +30,37 @@ class Solution {
             arr.push_back(neg[n++]);
          }
         }
+        //    for(int i=0;i<n/2;i++)
+        //     {
+        //        arr[2*i] = pos[i];
+        //        arr[2*i+1] = neg[i];
+        //     }
         return arr;
         }
     };
+
+    //optimal solve 
+    class Solution {
+        public:
+            vector<int> rearrangeArray(vector<int>& nums) {
+              int n=nums.size();
+              vector<int> arr(n);
+               
+              int pos=0;
+              int neg=1;
+             
+              for(int i=0;i<n;i++)
+              {
+               if(nums[i]>=0)
+               {
+                  arr[pos]=nums[i];
+                  pos+=2;
+               }
+               else{
+                 arr[neg]=nums[i];
+                  neg+=2;
+               }
+              }    
+              return arr;
+            }
+        };
