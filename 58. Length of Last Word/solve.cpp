7a69fb7f364501da8cd117fff747f62c86fa3c7b
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-04-27 22:18:26 +0600

    Update solve.cpp

--- 58. Length of Last Word/solve.cpp
+++ 58. Length of Last Word/solve.cpp
@@ -1,20 +1,15 @@
-#include <iostream>
-#include <string>
-using namespace std;
 
 class Solution {
 public:
     int lengthOfLastWord(string s) {
         int n = s.length();
         int counter = 0;
-
         // Start from the end and skip trailing spaces
         int i = n - 1;
         while (i >= 0 && s[i] == ' ') {
             i--;
         }
         n = i;
-
         // Count the length of the last word
         for (i = n; i >= 0; i--) {
             if (s[i] != ' ') {
@@ -27,14 +22,21 @@ public:
     }
 };
 
-int main() {
-    Solution solution;
-    string testString;
-
-    cout << "Enter a string: ";
-    getline(cin, testString);
-
-    int result = solution.lengthOfLastWord(testString);
-    cout << "The length of the last word is: " << result << endl;
-    return 0;
-}
+//using only one loop
+class Solution {
+    public:
+        int lengthOfLastWord(string s) {
+            int count=0;
+            for(int i=s.length()-1;i>=0;i--)
+            {
+                if(s[i]!=' ')
+                count++;
+                if(count>0)
+                {
+                    if(s[i]==' ')
+                    break;
+                }
+            }
+            return count;
+        }
+    };
