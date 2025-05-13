1f959d5d8c3802b51f3b0d6174d12df22bc493a1
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-05-13 22:34:07 +0600

    update

--- 73. Set Matrix Zeroes/solve.cpp
+++ 73. Set Matrix Zeroes/solve.cpp
@@ -54,5 +54,38 @@ void setZeroes(vector<vector<int>>& matrix) {
 // Finally, it sets all -1 values to 0.
 // The time complexity is O(n*m) for the first loop and O(n*m) for the second loop, resulting in O(n*m) overall.
 // The space complexity is O(1) since we are modifying the matrix in place.
-
-
+//Better approach
+class Solution {
+   public:
+       void setZeroes(vector<vector<int>>& matrix) {
+            int n = matrix.size();
+           int m = matrix[0].size();
+          vector<int> row(n, 0);  
+          vector<int> col(m, 0);
+           for(int i=0;i<n;i++)
+           {
+               for(int j=0;j<m;j++)
+               {
+                   if(matrix[i][j]==0)
+                   {
+                       row[i] = 1;
+                       col[j] = 1;
+                   }
+               }
+           }
+           for(int i=0;i<n;i++)
+           {
+               for(int j=0;j<m;j++)
+               {
+                   if(row[i]==1 || col[j]==1)
+                   {
+                       matrix[i][j]=0;
+                   }
+               }
+           }
+       }
+   };
+// This code is an Better solution to the problem of setting matrix zeroes.   
+// It uses two auxiliary vectors to keep track of which rows and columns need to be set to zero.
+// The time complexity is O(n*m) for the first loop and O(n*m) for the second loop, resulting in O(n*m) overall.
+// The space complexity is O(n+m) due to the use of two auxiliary vectors.
