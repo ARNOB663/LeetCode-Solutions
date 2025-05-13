efe987a24f1e7ba7a6a53975722823455b8d6cb9
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-05-13 23:16:34 +0600

    Update solve.cpp

--- 73. Set Matrix Zeroes/solve.cpp
+++ 73. Set Matrix Zeroes/solve.cpp
@@ -89,3 +89,57 @@ class Solution {
 // It uses two auxiliary vectors to keep track of which rows and columns need to be set to zero.
 // The time complexity is O(n*m) for the first loop and O(n*m) for the second loop, resulting in O(n*m) overall.
 // The space complexity is O(n+m) due to the use of two auxiliary vectors.
+
+// This is solve without using extra space
+class Solution {
+   public:
+       void setZeroes(vector<vector<int>>& matrix) {
+           int n = matrix.size();
+            int m = matrix[0].size();
+              int col0 =1;
+       //    vector<int> row(n, 0);  
+       //    vector<int> col(m, 0);
+           for(int i=0;i<n;i++)
+           {
+               for(int j=0;j<m;j++)
+               {
+                   if(matrix[i][j]==0)
+                   {
+                       matrix[i][0]=0;
+                       if(j !=0)
+                       matrix[0][j]=0;
+                       else
+                       col0=0;
+                   }
+               }
+           }
+           for(int i=1;i<n;i++)
+           {
+               for(int j=1;j<m;j++)
+               {
+                   if(matrix[i][j]!=0)
+                   {
+                       if( matrix[i][0] == 0 ||  matrix[0][j] == 0)
+                       {
+                           matrix[i][j] = 0;
+                       }
+   
+                   }
+               }
+           }
+           if(matrix[0][0] == 0)
+           {
+               for(int j=0;j<m;j++)
+               {
+                   matrix[0][j] = 0;
+               }
+           }
+           if(col0==0)
+           {
+               for(int i=0;i<n;i++)
+               {
+                   matrix[i][0] = 0;
+               }
+           }      
+       }
+   };
